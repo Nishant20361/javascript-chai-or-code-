650262a71a9b9ddf308b83bea7b885298f108787
Nishant20361 <nishantkmrdhn24@gmail.com>
2026-06-07 17:58:24 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all code as newer verison
+
+//alert(3 + 2) //we are using node js not browser
+
+console.log(3+
+    3)//code readability should be high
+
+console.log("nishant")
+
+let name = "Nishant"
+let age=21
+let isLoggedin = false
+
+// number => 2 to the power of 53
+// bigint (for large number)
+// string =""
+//boolean => true or false
+// null => stand alone value
+// undefined => 
+// symbol => unique 
+
+
+
+// object
+
+console.log(typeof "Nishant"); //  it shows string, int etc
+console.log(typeof age); //  it shows string, number etc
+console.log(typeof null); // show object
+console.log(typeof undefined); // show undefined
