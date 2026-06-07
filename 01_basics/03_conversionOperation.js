956ddd58a7fc9666b391e388ce5e69013c77cb4c
Nishant20361 <nishantkmrdhn24@gmail.com>
2026-06-07 18:29:11 +0000

    conversion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score)); // both are same 
+
+let valueInNumber = Number(score) // it convert number to string
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+//"33"=33
+//"33abc"=NaN (not a number)
+// true =1 or false = 0
+
+let isLoggedIn=1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn );
+
+// 1 = true ; 0= false
+//"Nishant" => True ; " " => false
+
+
+let someNumber = 1223
+console.log(typeof someNumber);
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
