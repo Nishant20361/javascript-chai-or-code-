e2fc97e32d9fa3d3d6ee568bd2c04aa1b11975d6
Nishant20361 <nishantkmrdhn24@gmail.com>
2026-06-08 20:04:05 +0000

    converison operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,3 +25,36 @@ console.log(typeof someNumber);
 let stringNumber = String(someNumber)
 console.log(typeof stringNumber);
 
+//************************ Operation ********************************** */
+
+let value=3
+let negValue= -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); //power
+// console.log(2/2);
+// console.log(3%2);
+
+
+let str1 = "Nishant"
+let str2 = " Kumar"
+let str3 = str1+str2
+console.log(str3);
+
+// console.log(2+ "1");
+// console.log("2"+ 1);
+// console.log(2+1+ "1");
+// console.log("2"+1+ 1);
+
+console.log(+true);
+
+let num1 ,num2,num3;
+ num1=num2=num3 =2+2
+
+ let gameCounter= 100
+ gameCounter++;
+ console.log(gameCounter);
+ 
